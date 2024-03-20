10d8aea65db10d9f7d1358b23cb972308fb2610d
MirellaAguiar <mirellapiresaguiar@gmail.com>
2024-03-20 09:51:35 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,14 +1,36 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
-var nome = document.getElementById("nome");
+var nome        = document.getElementById("nome");
+var email       =  document.getElementById("email");
+var telefone    =  document.getElementById("telefone");
+var cep         =  document.getElementById("cep");
+var estado      =  document.getElementById("estado");
+var cidade      =  document.getElementById("cidade");
+var bairro      =  document.getElementById("bairro");
+var logradouro  =  document.getElementById("logradouro");
+var numero      =  document.getElementById("numero");
+var complemento =  document.getElementById("complemento");
 
-function alertar(event)
-{ alert("Voce clicou no botao!!!" + " " + nome.value);
+var saida = document.getElementById("saida-de-dados");
 
-var numero = 7;
-    var resultado = numero % 2;
-    if(resultado == 0){
-        alert("este número é par!");
+function alertar(event){
+//{ alert("Voce clicou no botao!!!" + " " + nome.value);
 
-    }
+//var numero = 7;
+   // var resultado = numero % 2;
+   // if(resultado == 0){
+    //    alert("este número é par!");
+
+   // }
+   saida.inertText = "Nome:  "+ nome.value +
+   "\n Email:  " + email.value;
+   "\n telefone:  " + telefone.value;
+   "\n cep:  " + cep.value;
+   "\n estado:  " + estado.value;
+   "\n cidade:  " + cidade.value;
+   "\n bairro:  " + bairro.value;
+   "\n logradouro:  " + logradouro.value;
+   "\n numero:  " + numero.value;
+   "\n comlemento:  " + complemento.value;
+   
 }
